bdf344744bb11f0fc77ae417a5d4893faae49c00
Connor McKelvey <connormmckelvey@gmail.com>
2025-02-28 12:36:20 -0600

    completed prob00-01

---
+++ finished/prob01.py
@@ -0,0 +1,4 @@
+with open("input.txt", "r") as file:
+    name = file.readline().rstrip()
+
+print("It is by will alone I set my mind in motion.\nIt is by the juice of sapho that thoughts acquire speed,\nthe lips acquire stains.\nThe stains become a warning.\nIt is by will alone I set my mind in motion.\nI'm going to let you in on a secret, "+name+".\nThings aren't going so well for the Harkonnen.\nAre we to sit back and watch as the Emperor gives away Spice that is rightfully ours?\nCheer as the Atreides take our Imperial Basin?\nGo. Take some men and mine everything.")
